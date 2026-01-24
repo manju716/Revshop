3d003f18342cc5987797038c77757afa2f3fdec5
manju716 <135322588+manju716@users.noreply.github.com>
2026-01-24 11:38:05 +0530

    Create DatabaseConfig.java

---
+++ config/DatabaseConfig.java
@@ -0,0 +1,13 @@
+package com.config;
+
+
+public class DatabaseConfig {
+
+    public static final String URL =
+            "jdbc:mysql://localhost:3306/revshop";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "Manjula@0806";
+}
+
+
+
